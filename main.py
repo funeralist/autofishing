ab6fab06293ea4a4f91ea61be2dcd964909b86c8
Taken <49839074+ilytaken@users.noreply.github.com>
2023-05-30 21:42:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,27 +6,30 @@ import numpy
 import keyboard
 import config
 
+is_start_of_fishing = False
+image = None
+activity_state = False
+
 
 def hold_e():
     pyautogui.keyDown(config.interaction_key)
     time.sleep(0.6)
     pyautogui.keyUp(config.interaction_key)
+    global is_start_of_fishing
+    is_start_of_fishing ^= True
 
-
-image = None
-activity_state = False
-
-print('Macro launched!')
 
 with mss.mss() as sct:
     while True:
         # check for the hotkey
         if keyboard.is_pressed(config.hotkey):
             activity_state ^= True
-            print('Macro is ' + (not activity_state) * 'not ' + 'working!')
-            time.sleep(0.7)
+            if activity_state:
+                is_start_of_fishing = True
+            print('Macro is ' + (not activity_state) * 'not ' + 'working.')
+            time.sleep(0.7)  # to prevent multi-toggling
 
-        # if hotkey was pressed
+        # if activity_state is on True
         if activity_state:
             # get screenshot and convert to HSV color format
             image = numpy.asarray(sct.grab(config.capture))
@@ -40,4 +43,6 @@ with mss.mss() as sct:
             # check for white pixels
             has_color = numpy.sum(mask)
             if has_color > 0:
+                if not is_start_of_fishing:
+                    time.sleep(config.catch_delay)
                 hold_e()
